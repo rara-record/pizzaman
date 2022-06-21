c9045aab18f22261334c6f4c00d521fee6bd9ec0
bora <a52447879@gmail.com>
2022-06-22 02:38:17 +0900

    [feat] Home 페이지 feature-section UI 추가 (featureData Mock Data 추가)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,29 @@ import { Container, Row, Col } from 'reactstrap'
 import heroImg from '../assets/images/hero.png'
 import Category from '../components/UI/Category'
 
+import featureImg01 from '../assets/images/service-01.png'
+import featureImg02 from '../assets/images/service-02.png'
+import featureImg03 from '../assets/images/service-03.png'
+
+const featureData = [
+  {
+    title: 'Quick Delivery',
+    imgUrl: featureImg01,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.',
+  },
+
+  {
+    title: 'Super Dine In',
+    imgUrl: featureImg02,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.',
+  },
+  {
+    title: 'Easy Pick Up',
+    imgUrl: featureImg03,
+    desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus, doloremque.',
+  },
+]
+
 const Home = () => {
   return (
     <Helmet title="Home">
@@ -62,8 +85,52 @@ const Home = () => {
         </Container>
       </section>
 
-      <section className="pt-0">
+      <section className="category pt-0">
         <Category />
+      </section>
+
+      <section className="feature">
+        <Container>
+          <Row>
+            <Col lg="12">
+              <h5 className="feature__subtitle mb-4">What we serve</h5>
+              <h2 className="feature__title">Just sit back at home</h2>
+              <h2 className="feature__title">
+                we will <span>take care</span>
+              </h2>
+
+              <p className="mb-1 mt-4 feature__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
+                officiis?
+              </p>
+
+              <p className="feature__text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Aperiam, eius.
+              </p>
+            </Col>
+
+            {featureData.map((item, index) => (
+              <Col
+                lg="4"
+                md="6"
+                sm="6"
+                key={index}
+                className="mt-5 text-center px-5 py-3"
+              >
+                <div className="feature__item">
+                  <img
+                    src={item.imgUrl}
+                    alt="feature-img"
+                    className="w-25 mb-3"
+                  />
+                  <h5 className="fw-bold mb-3">{item.title}</h5>
+                  <p>{item.desc}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </section>
     </Helmet>
   )
