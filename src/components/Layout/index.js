20c81d7cc8d5ee3e6e144de7cedce56f2224f3c4
bora <a52447879@gmail.com>
2022-06-25 03:11:29 +0900

    [feat] Cart UI 추가

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
-
 import Header from '../Header'
 import Footer from '../Footer'
 import Routers from '../../routes'
+
+import Carts from '../UI/Carts'
 
 const Layout = () => {
   return (
     <div>
       <Header />
+      <Carts />
       <div>
         <Routers />
       </div>
