cab30a8b33124c0f8fec2f04679ac4b5d0d4de40
bora <a52447879@gmail.com>
2022-06-25 03:31:38 +0900

    [feat] redux 세팅

--- src/index.js
+++ src/index.js
@@ -10,10 +10,15 @@ import './style.css'
 
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import store from './store'
+import { Provider } from 'react-redux'
+
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root')
