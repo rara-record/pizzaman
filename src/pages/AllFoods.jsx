d71ced13c472d3fbf63fc9bac3e7a6db81668290
BORA <a52447879@gmail.com>
2022-07-05 23:38:50 +0900

    [feat] All Foods UI 추가

--- src/pages/AllFoods.jsx
+++ src/pages/AllFoods.jsx
@@ -1,7 +1,51 @@
 import React from 'react'
+import Helmet from '../components/Helmet'
+import CommonSection from '../components/UI/CommonSection'
+
+import { Container, Col, Row } from 'reactstrap'
+
+import products from '../assets/fake-data/products'
+import ProductCard from '../components/UI/ProductCard'
 
 const AllFoods = () => {
-  return <div>AllFoods</div>
+  return (
+    <Helmet title="All-Foods">
+      <CommonSection title="All Foods" />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="6" xs="12">
+              <div className="search__widget d-flex align-items-center justify-content-between">
+                <input type="text" placeholder="I'm looking for...." />
+                <span>
+                  <i className="ri-search-line"></i>
+                </span>
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="6" xs="12" className="mb-5">
+              <div className="sorting__widget text-end">
+                <select className="w-50">
+                  <option>Default</option>
+                  <option value="ascending">Alphabetically, A-Z</option>
+                  <option value="descending">Alphabetically, Z-A</option>
+                  <option value="high-price">High Price</option>
+                  <option value="low-price">Low Price</option>
+                </select>
+              </div>
+            </Col>
+
+            {products.map((item) => (
+              <Col lg="3" md="4" sm="6" xs="6" key={item.id}>
+                <ProductCard item={item} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  )
 }
 
 export default AllFoods
